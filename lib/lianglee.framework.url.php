6939180c8190756524ab3aa1d479774d14dd5f02
unknown <Administrator@buddyexp-71007d.(none)>
2012-10-28 23:13:19 +0500

    Lianglee_url_contain() @todo add a message or log

---
+++ lib/lianglee.framework.url.php
@@ -0,0 +1,27 @@
+<?php
+/* LiangLee Framework
+ * @website Link: http://community.elgg.org/pg/profile/arsalanlee/
+ * FrameWork for Liang Lee Plugins
+ * @package LiangLeeFramework 
+ * @author Liang Lee
+ * @copyright All right reserved Liang Lee 2012.
+ * @File lianglee.framework.url.php
+ */
+ 
+/*
+ * LiangLeeFramework Url , return true of url contain a sting
+ *
+ * @uses Lianglee_url_contain(array('string' => '<string>','value' => '<value>'));
+ * @access system
+ */
+ 
+function Lianglee_url_contain($params = array()){
+if(isset($params) && !empty($params)){
+   if(strstr($params['string'], $params['value'])) { 
+       return true;
+} if(empty($params)){
+  $error = 'params not be empty';
+  return false;
+   }
+
+} 
