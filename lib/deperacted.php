a245df3c1617337bfa868cf6d47f4b6148eb7312
unknown <manactric@gmail.com>
2012-10-28 23:19:02 +0500

    deperacted functions not yet added long term support

---
+++ lib/deperacted.php
@@ -0,0 +1,9 @@
+<?php
+/* LiangLee Framework
+ * @website Link: http://community.elgg.org/pg/profile/arsalanlee/
+ * FrameWork for Liang Lee Plugins
+ * @package LiangLeeFramework 
+ * @author Liang Lee
+ * @copyright All right reserved Liang Lee 2012.
+ * @File LiangLeeFramework.php
+ */
