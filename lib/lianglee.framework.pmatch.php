66b3aa2e6e14e2084e92f2c28f6623012f1066d3
unknown <manactric@gmail.com>
2012-10-28 23:17:16 +0500

    match a specific character in a string using regex

---
+++ lib/lianglee.framework.pmatch.php
@@ -0,0 +1,25 @@
+<?php
+/* LiangLee Framework
+ * @website Link: http://community.elgg.org/pg/profile/arsalanlee/
+ * FrameWork for Liang Lee Plugins
+ * @package LiangLeeFramework 
+ * @author Liang Lee
+ * @copyright All right reserved Liang Lee 2012.
+ * @File lianglee.framework.pmatch.php
+ */
+ 
+/*
+ * Liang Lee Framework Pregmatch Lib
+ *
+ * @uses Lianglee_pmatch_reg(array('regex' => <regexstring>, 'string' => <stringtomatch>));
+ * @access system
+ */
+function Lianglee_pmatch_reg($params = array()){
+  if(isset($params['regex'])){
+     if(!empty($params['regex'] && !empty($params['string']))){
+        if(preg_match ($params['regex'], $params['string'], $matches, PREG_OFFSET_CAPTURE)){
+         return true;
+        }
+     }
+  } 	
+}  
