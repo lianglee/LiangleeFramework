d7cc0814462a264868864346a7d48b2bf9d26cc6
unknown <Administrator@buddyexp-71007d.(none)>
2012-10-28 23:14:08 +0500

    get request url with connection,host

---
+++ lib/lianglee.framework.requrl.php
@@ -0,0 +1,23 @@
+<?php
+/* LiangLee Framework
+ * @website Link: http://community.elgg.org/pg/profile/arsalanlee/
+ * FrameWork for Liang Lee Plugins
+ * @package LiangLeeFramework 
+ * @author Liang Lee
+ * @copyright All right reserved Liang Lee 2012.
+ * @File lianglee.framework.requrl.php
+ */
+
+/*
+ * Get a current url
+ * @uses  Lianglee_requested_url_full(true); or echo Lianglee_requested_url_full(true); depends on you
+ * @access system
+ */
+function Lianglee_requested_url_full($params){
+   if(isset($params)){
+       if($params == true){
+        $url = Lianglee_get_connection().LiangLee_server('SERVER_NAME').LiangLee_server('REQUEST_URI');
+        return $url;
+       }
+	}   
+}
