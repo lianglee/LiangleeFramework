0885a4354e11ed5d3707552413b9f84bc3b95b54
lianglee <manactric@gmail.com>
2013-01-20 02:16:20 -0800

    acesss premission

--- lib/lianglee.framework.instaldir.php
+++ lib/lianglee.framework.instaldir.php
@@ -7,9 +7,10 @@
  * @copyright All right reserved Liang Lee 2012.
  * @File lianglee.framework.installdir.php
  */
- 
+ defined('_LEE_EXEC') or die ('Restricted access');  
+
 /*
- * Get a LiangleeFramework_BASEURL (host, scheme, path)
+ * Get a lee_baseurl (host, scheme, path)
  * @uses = Lianglee_get_my_elgg('host'); <= displays site name
  * @uses = Lianglee_get_my_elgg('scheme'); <= displays scheme
  * @uses = Lianglee_get_my_elgg('path'); <= displays installed subdirectory name if defined
@@ -17,8 +18,8 @@
  * @access system
  */
 function Lianglee_get_my_elgg($params){
-$url = LiangleeFramework_BASEURL;
-$fetch = parse_url(LiangleeFramework_BASEURL);
+$url = lee_baseurl;
+$fetch = parse_url(lee_baseurl);
 if($params == 'host'){
    if(isset($fetch)){
    $return = $fetch['host'];
